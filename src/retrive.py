d8cf92745fbc03b8b620ca95b0eeafc85765cbde
LMigliet <lmigliet@ic.ac.uk>
2024-08-09 21:45:48 +0100

    add doc strings

--- src/retrive.py
+++ src/retrive.py
@@ -3,8 +3,18 @@ import pandas as pd
 from src.components import Param, Well
 
 
-# Helper function to parse Params
-def parse_params(params_df, well_id, nmeta):
+def parse_params(params_df: pd.DataFrame, well_id: str, nmeta: int):
+    """
+    Parse parameters from a DataFrame for a specific well.
+
+    Args:
+        params_df (pd.DataFrame): DataFrame containing parameters with wells as index.
+        well_id (str): Identifier of the well for which parameters are to be parsed.
+        nmeta (int): Number of metadata columns before the actual parameter columns.
+
+    Returns:
+        List[Param]: A list of Param objects, each representing a parameter with a name and value.
+    """
     params = []
     df = params_df.iloc[:, nmeta:].copy()
     for col in df.columns:
@@ -14,8 +24,21 @@ def parse_params(params_df, well_id, nmeta):
     return params
 
 
-# Helper function to parse Wells
-def parse_wells(ac_df, params_df, well_type, nmeta=5):
+def parse_wells(
+    ac_df: pd.DataFrame, params_df: pd.DataFrame, well_type: str, nmeta: int = 5
+):
+    """
+    Parse wells and their associated data from given DataFrames.
+
+    Args:
+        ac_df (pd.DataFrame): DataFrame containing well data with intensities and other metrics.
+        params_df (pd.DataFrame): DataFrame containing parameters associated with each well.
+        well_type (str): Type of the wells being parsed (e.g., "control", "sample").
+        nmeta (int, optional): Number of metadata columns before the actual data columns. Defaults to 5.
+
+    Returns:
+        List[Well]: A list of Well objects, each representing a well with its id, type, intensities, and parameters.
+    """
     wells = []
     for well_id, row in ac_df.iterrows():
         intensities = row[nmeta:].tolist()
